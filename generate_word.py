11ff72b88a4bf47c171bad13a72a3b6e72696dcd
Neel Patel <neel.patel6573@gmail.com>
2019-05-07 16:58:43 +0530

    add generate word list code

---
+++ generate_word.py
@@ -0,0 +1,15 @@
+import sys
+import itertools
+try:
+    from itertools import imap
+except ImportError:
+    imap=map
+
+string = sys.argv[1]
+length = sys.argv[2]
+
+file = open("word-list.txt","w+")
+a = []
+for i in imap(''.join, itertools.product(string, repeat=int(length))):
+    a.append(i+"\n")
+file.writelines(a)
