d469542da1474f2f72e42b24f2ab2130d35f2113
Neel Patel <neel.patel6573@gmail.com>
2019-05-10 21:21:54 +0530

    add one

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func cleanup() {
@@ -13,8 +15,35 @@ func cleanup() {
 	}
 }
 
-func one(reader *bufio.Reader) {
+func bruteforce(filename string, reader *bufio.Reader) {
 
+}
+
+func one(reader *bufio.Reader) {
+	defer cleanup()
+
+	fmt.Print("$: Enter the string: ")
+	string, err := reader.ReadString('\n')
+	string = strings.TrimSpace(string)
+	if err!=nil {
+		panic(err.Error())
+	}
+	fmt.Print("$: Enter the length of password: ")
+	length, err := reader.ReadString('\n')
+	if err!=nil {
+		panic(err.Error())
+	}
+
+	cmd := exec.Command("python", "generate_word.py", string, length)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err!=nil {
+		panic(err.Error())
+	}
+
+	filename := "word-list.txt"
+	bruteforce(filename, reader)
 }
 
 func two(reader *bufio.Reader) {
@@ -28,7 +57,7 @@ func main() {
 
 	for {
 		fmt.Print("$: 1. Create wordlist and attack 2. Use own wordlist and attack 3. Exit - press Ctrl + c $: ")
-		choice,err := reader.ReadString("\n")
+		choice,err := reader.ReadString('\n')
 		if err!=nil {
 			panic(err.Error())
 		}
